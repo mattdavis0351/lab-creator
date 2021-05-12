bd093cf4d7e0580cd6c7136a93bcf672e79bcf4b
mattdavis0351 <mattdavis0351@github.com>
2021-05-12 13:58:38 -0700

    add initial program command

--- lab-creator.js
+++ lab-creator.js
@@ -2,7 +2,25 @@ const chalk = require("chalk");
 const commander = require("commander");
 
 async function init() {
-  console.log(chalk.yellow("it works"));
+  // Create new commander.Command()
+  // command new-lab
+  // option --lab-name
+  // lab-creator new-lab --lab-name bread
+  const program = new commander.Command("lab-creator");
+  // program metadata
+  program.version("0.0.1");
+
+  program
+    .command("new-lab")
+    .description("Create a new lab")
+    .requiredOption("-l,--lab-name <lab-name>")
+    .action((args) => {
+      // use nunjucks to fill in templates that are going to be copied
+      // put in separate function for readability
+      console.log(`args: ${args.labName}`);
+    });
+
+  program.parse(process.argv);
 }
 
 module.exports = { init };
