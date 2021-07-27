d9f005823343b8d59b4aaaa8b4906f5e40937776
mattdavis0351 <mattdavis0351@github.com>
2021-07-27 08:35:51 -0700

    add makerequireddirs function and action name flag

--- src/githubExerciseManager.js
+++ src/githubExerciseManager.js
@@ -1,4 +1,6 @@
 const commander = require("commander");
+const { fstat } = require("fs-extra");
+const tmp = require("tmp-promise");
 const packageJSON = require("../package.json");
 const getLatestLookingGlassVersion = require("./checkForUpdates");
 const findTemplateFiles = require("./findTemplateFiles");
@@ -10,30 +12,46 @@ async function init(){
         .description("Create a new exercise")
         .requiredOption("-n,--exercise-name <exercise-name>")
         .option("-v,--lg-version <looking-glass-version>","Looking Glass version you wish to use in the grading.yml exercise file",await getLatestLookingGlassVersion())
+        .option("-a,--action-name <local-action-name>","User defined name for the local action of the exercise", "local-action")
         .action(async (options) => {
             const exerciseName = options.exerciseName;
             const lgVersion = options.lgVersion 
-            const exercise = await createExercise(exerciseName, lgVersion);
+            const actionName = options.actionName;
+            const exercise = await createExercise(exerciseName, lgVersion,actionName);
             console.log(`Exercise ${exercise.name} created`);
             
         });
     program.parse(process.argv);
     }
 
-async function createExercise(exerciseName, lgVersion){
+async function createExercise(exerciseName, lgVersion,actionName){
     const exercise = {
         name: exerciseName,
         lgVersion: lgVersion,
+        actionName: actionName,
         files: [],
         tests: []
     };
 
+    const tempPath = await tmp.dir({unsafeCleanup: true});
 
-    exercise.files = findTemplateFiles(`${tempDir.path}/node_modules/${packageJSON.name}/templates`)
+    exercise.files = findTemplateFiles(`${tempPath.path}/node_modules/${packageJSON.name}/templates`)
 
     return exercise;
 
 }
+
+async function makeRequiredDirs(exercise){
+    const requiredDirs = [
+        `${process.cwd()}/.github/workflows`,
+        `${process.cwd()}/.github/actions/${exercise.actionName}`
+    ];
+    for(const dir of requiredDirs){
+        await fs.ensureDir(dir);
+    }
+}
+
+init();
 
 
 
