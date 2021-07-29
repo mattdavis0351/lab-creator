d6f2b27599add13affb82b4aa6f2380cdb9c78db
mattdavis0351 <mattdavis0351@github.com>
2021-07-29 16:21:59 -0700

    fix typo

--- src/githubExerciseManager.js
+++ src/githubExerciseManager.js
@@ -4,7 +4,7 @@ const tmp = require("tmp-promise");
 const { spawn } = require("child_process");
 const packageJSON = require("../package.json");
 const getLatestLookingGlassVersion = require("./checkForUpdates");
-const findTemplateFiles,flattenTemplateFilesArray,renderTemplates = require("./findTemplateFiles");
+const findTemplateFiles,flattenTemplateFilesArray,renderTemplates = require("./templates");
 
 async function init() {
   const program = new commander.Command("ghexmgr");
