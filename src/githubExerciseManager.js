4f6c5eafa6790621df15642fbf2b21c6ff22ed75
mattdavis0351 <mattdavis0351@github.com>
2021-07-29 13:05:20 -0700

    flatten template files array

--- src/githubExerciseManager.js
+++ src/githubExerciseManager.js
@@ -1,85 +1,101 @@
 const commander = require("commander");
 const fs = require("fs-extra");
 const tmp = require("tmp-promise");
-const {spawn} = require("child_process");
+const { spawn } = require("child_process");
 const packageJSON = require("../package.json");
 const getLatestLookingGlassVersion = require("./checkForUpdates");
-const findTemplateFiles = require("./findTemplateFiles");
+const findTemplateFiles,flattenTemplateFilesArray = require("./findTemplateFiles");
 
-async function init(){
-    const program = new commander.Command("ghexmgr");
-    program.version(packageJSON.version);
-    program.command("new-exercise")
-        .description("Create a new exercise")
-        .requiredOption("-n,--exercise-name <exercise-name>")
-        .option("-v,--lg-version <looking-glass-version>","Looking Glass version you wish to use in the grading.yml exercise file",await getLatestLookingGlassVersion())
-        .option("-a,--action-name <local-action-name>","User defined name for the local action of the exercise", "local-action")
-        .action(async (options) => {
-            const exerciseName = options.exerciseName;
-            const lgVersion = options.lgVersion 
-            const actionName = options.actionName;
-            const exercise = await createExercise(exerciseName, lgVersion,actionName);
-            console.log(`Exercise ${exercise.name} created`);
-            
-        });
-    program.parse(process.argv);
-    }
-
-async function createExercise(exerciseName, lgVersion,actionName){
-    const exercise = {
-        name: exerciseName,
-        lgVersion: lgVersion,
-        actionName: actionName,
-        files: [],
-        tests: []
-    };
-
-    exercise.tempPath = await tmp.dir({unsafeCleanup: true});
-    await makeRequiredDirs(exercise);
-
-    exercise.files = findTemplateFiles(`${exercise.tempPath.path}/node_modules/${packageJSON.name}/templates`)
-
-    return exercise;
-
+async function init() {
+  const program = new commander.Command("ghexmgr");
+  program.version(packageJSON.version);
+  program
+    .command("new-exercise")
+    .description("Create a new exercise")
+    .requiredOption("-n,--exercise-name <exercise-name>")
+    .option(
+      "-v,--lg-version <looking-glass-version>",
+      "Looking Glass version you wish to use in the grading.yml exercise file",
+      await getLatestLookingGlassVersion()
+    )
+    .option(
+      "-a,--action-name <local-action-name>",
+      "User defined name for the local action of the exercise",
+      "local-action"
+    )
+    .action(async (options) => {
+      const exerciseName = options.exerciseName;
+      const lgVersion = options.lgVersion;
+      const actionName = options.actionName;
+      const exercise = await createExercise(
+        exerciseName,
+        lgVersion,
+        actionName
+      );
+      console.log(`Exercise ${exercise.name} created`);
+    });
+  program.parse(process.argv);
 }
 
-async function makeRequiredDirs(exercise){
-    const requiredDirs = [
-        `${process.cwd()}/.github/workflows`,
-        `${process.cwd()}/.github/actions/${exercise.actionName}`
-    ];
-    for(const dir of requiredDirs){
-        await fs.ensureDir(dir);
-    }
+async function createExercise(exerciseName, lgVersion, actionName) {
+  const exercise = {
+    name: exerciseName,
+    lgVersion: lgVersion,
+    actionName: actionName,
+    files: [],
+    tests: [],
+  };
+
+  exercise.tempPath = await tmp.dir({ unsafeCleanup: true });
+  await makeRequiredDirs(exercise);
+
+  await npmInstall(exercise);
+
+  const templateFiles = findTemplateFiles(
+    `${exercise.tempPath.path}/node_modules/${packageJSON.name}/templates`
+  );
+
+  // flatten the templateFiles array and stroe in exercise.files
+  exercise.files = flattenTemplateFilesArray(templateFiles);
+
+  exercise.tempPath.cleanup();
+  return exercise;
+}
+
+async function makeRequiredDirs(exercise) {
+  const requiredDirs = [
+    `${process.cwd()}/.github/workflows`,
+    `${process.cwd()}/.github/actions/${exercise.actionName}`,
+  ];
+  for (const dir of requiredDirs) {
+    await fs.ensureDir(dir);
+  }
 }
 
 // copy files from one location to another
-async function copyFiles(exercise){
-    // spawn npm and install into temp dir
-    
-    
-    const npm = spawn("npm", ["install","--prefix",exercises.tempPath.path,packageJSON.name]);
-    npm.stdout.on("data", (data) => {
-        console.log(data.toString());
-    });
+async function npmInstall(exercise) {
+  // spawn npm and install into temp dir
 
-    npm.on("close", async (code) => {
-    if(code === 0){
+  const npm = spawn("npm", [
+    "install",
+    "--prefix",
+    `${exercise.tempPath.path}`,
+    `${packageJSON.name}`,
+  ]);
 
-    
-    for(const file of exercise.files){
-        // const to = something
-        await fs.copy(file,to);
-        
+  npm.stdout.on("data", (data) => {
+    console.log(data.toString());
+  });
+
+  npm.on("close", async (code) => {
+    if (code !== 0) {
+      console.log("npm install failed");
+      process.exit(code);
     }
-}});
-
-
-
-
+  });
+}
 
 // remove init once we have a working version
 init();
 
-
-module.exports = {init};
+module.exports = { init };
--- src/templates.js
+++ src/templates.js
@@ -1,18 +1,46 @@
-const fs = require('fs-extra')
+const fs = require("fs-extra");
+const path = require("path");
 
 // recursively find all files in a directory
 // returns an array of files
 function findTemplateFiles(dir) {
-    return fs.readdirSync(dir)
-      .map((file) => {
-        const filePath = `${dir}/${file}`;
-        if (fs.statSync(filePath).isDirectory()) {
-          return findFiles(filePath);
-        } else  {
-          return filePath;
-        }
-      })
-      .filter((file) => file !== undefined);
-  }
+  return fs
+    .readdirSync(dir)
+    .map((file) => {
+      const filePath = `${dir}/${file}`;
+      if (fs.statSync(filePath).isDirectory()) {
+        return findFiles(filePath);
+      } else {
+        return filePath;
+      }
+    })
+    .filter((file) => file !== undefined);
+}
 
-  module.exports = findTemplateFiles
+// flatten a nested array
+function flattenTemplateFilesArray(arr) {
+  return arr.reduce(
+    (a, b) => a.concat(Array.isArray(b) ? flattenTemplateFilesArray(b) : b),
+    []
+  );
+}
+// use nunjucks renderString to fill in contents of an array of files
+
+function renderTemplates(exercise, context) {
+  for (const file of exercise.files) {
+    const filePath = `${__dirname}/${file}`;
+    const template = fs.readFileSync(filePath, "utf8");
+    const fileContent = nunjucks.renderString(template, context);
+    const newPath = path
+      .parse(file)
+      .dir.replace(
+        `${exercise.tempPath.path}/node_modules/${packageJSON.name}/templates`,
+        `${process.cwd()}`
+      );
+    fs.writeFileSync(newPath, fileContent);
+  }
+}
+
+module.exports = findTemplateFiles;
+module.exports = flattenTemplateFilesArray;
+module.exports = renderTemplates;
